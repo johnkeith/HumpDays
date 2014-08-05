5ddf135863206234d4c1d4ea405526359b5d8e6c
John Keith <johnkeith@outlook.com>
2014-08-05 16:28:07 -0400

    feature for sign up; capybara in rails helper

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "User signs up for HumpDays" do
+  # As a user, I want to be able to sign up using my Facebook or Twitter account,
+  # so that sign up is simple.
+  scenario "successful sign up using email" do
+  end
+
+  scenario "failed sign up using email" do
+  end
+  
+  scenario "successful sign up using Facebook" do
+  end
+
+  scenario "failed sign up using Facebook" do
+  end
+
+  scenario "successful sign up using Twitter" do
+  end
+
+  scenario "failed sign up using Twitter" do
+  end
+end
